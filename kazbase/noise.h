20c1a356662f8413782c7aa77839be3cb1b38031
Luke Benstead <kazade@gmail.com>
2014-05-24 10:56:21 +0100

    Add some perlin noise functions

---
+++ kazbase/noise.h
@@ -0,0 +1,36 @@
+#ifndef NOISE_H
+#define NOISE_H
+
+#include <random>
+#include <array>
+
+namespace noise {
+
+class Perlin {
+public:
+    Perlin(uint32_t seed=std::mt19937::default_seed);
+
+    double noise(double x) const { return noise(x, 0, 0); }
+    double noise(double x, double y) const { return noise(x, y, 0); }
+    double noise(double x, double y, double z) const;
+
+private:
+    std::array<int, 512> p;
+};
+
+class PerlinOctave {
+public:
+    PerlinOctave(int octaves, uint32_t seed=std::mt19937::default_seed);
+
+    double noise(double x) const { return noise(x, 0, 0); }
+    double noise(double x, double y) const { return noise(x, y, 0); }
+    double noise(double x, double y, double z) const;
+
+private:
+    Perlin perlin_;
+    int octaves_;
+};
+
+}
+
+#endif // NOISE_H
