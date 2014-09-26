6244fc2fa0ad6832fbcc43fe382d8af372212e91
Luke Benstead <kazade@gmail.com>
2014-09-26 20:16:46 +0100

    Make things compile on Android \o/

--- Android.mk
+++ Android.mk
@@ -1,8 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE	:= kazbase
-LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*/*.cpp)
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/kazbase/*.cpp)
+LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/kazbase/os/*.cpp)
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_CFLAGS	:= "-std=c++11"
 LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_LDLIBS += -latomic
+LOCAL_SHARED_LIBRARIES := libpcre
 include $(BUILD_SHARED_LIBRARY)
